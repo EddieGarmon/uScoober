6d6b8a0ef9b14b80c358dfc1a1341630cb958102
Eddie Garmon <eddie_garmon@hotmail.com>
2015-03-18 12:23:36 -0400

    stop sharing AssemblyInfo.cs in shared projects

--- Build/PoshTypeDefinitions/DebugEntryPoint.cs
+++ Build/PoshTypeDefinitions/DebugEntryPoint.cs
@@ -1,8 +1,9 @@
 ﻿namespace PoshTypeDefinitions
 {
-    public class EntryPoint
+    public class DebugEntryPoint
     {
         public static void Main() {
+            // This project exists to validate the c# that psake uses for version management
             SemVer semanticVersion = null;
             semanticVersion = SemVer.Parse("1.2.3");
             semanticVersion = SemVer.Parse("1.2.3-pre");
