b395e3f90f317b8fb699a7ee50bcb2616390051a
Eddie Garmon <eddie_garmon@hotmail.com>
2015-03-25 00:54:00 -0400

    updated Netduino APIs

--- Hardware/Boards.NetduinoPlus2/NetduinoPlus2.cs
+++ Hardware/Boards.NetduinoPlus2/NetduinoPlus2.cs
@@ -1,10 +1,11 @@
-﻿//using AnalogInput = uScoober.IO.Spot.AnalogInput;
-using System;
+﻿using System;
+using System.Collections;
 using Microsoft.SPOT.Hardware;
 using NetduinoPlus2.Tests;
 using uScoober.Hardware.I2C;
 using uScoober.Hardware.Input;
 using uScoober.Hardware.Light;
+using uScoober.Hardware.Spi;
 using uScoober.Hardware.Spot;
 using SL = SecretLabs.NETMF.Hardware.NetduinoPlus;
 
@@ -13,17 +14,32 @@ namespace uScoober.Hardware.Boards
     internal class NetduinoPlus2 : DisposableBase
     {
         private AnalogInputs _analogIn;
+        private DigitalInputs _digitalIn;
+        private DigitalOutputs _digitalOut;
         private II2CBus _i2CBus;
         private PushButton _onboardButton;
         private AnalogLed _onboardLed;
         private PwmOutputs _pwmOut;
+        private ISpiBus _spiBus;
 
         public AnalogInputs AnalogIn {
             get { return _analogIn ?? (_analogIn = new AnalogInputs()); }
         }
 
+        public DigitalInputs DigitalIn {
+            get { return _digitalIn ?? (_digitalIn = new DigitalInputs()); }
+        }
+
+        public DigitalOutputs DigitalOut {
+            get { return _digitalOut ?? (_digitalOut = new DigitalOutputs()); }
+        }
+
         public II2CBus I2CBus {
             get { return _i2CBus ?? (_i2CBus = new SpotI2CBus()); }
+        }
+
+        public ISpiBus ISpiBus {
+            get { return _spiBus ?? (_spiBus = new SpotSpiBus(SL.SPI_Devices.SPI1)); }
         }
 
         public PushButton OnboardButton {
@@ -49,117 +65,113 @@ namespace uScoober.Hardware.Boards
             }
         }
 
-        //todo: expose SpiBus
         //todo: expose storage entrypoint (via storage pack?)
         //todo: expose ethernet entrypoint? (via network pack?)
 
         public sealed class AnalogInputs
         {
-            private IAnalogInput _a0;
-            private IAnalogInput _a1;
-            private IAnalogInput _a2;
-            private IAnalogInput _a3;
-            private IAnalogInput _a4;
-            private IAnalogInput _a5;
+            private readonly IAnalogInput[] _store = new IAnalogInput[6];
 
             public IAnalogInput A0 {
-                get { return _a0 ?? (_a0 = new SpotAnalogInput(Cpu.AnalogChannel.ANALOG_0)); }
+                get { return GetOrCreate(0); }
             }
 
             public IAnalogInput A1 {
-                get { return _a1 ?? (_a1 = new SpotAnalogInput(Cpu.AnalogChannel.ANALOG_1)); }
+                get { return GetOrCreate(1); }
             }
 
             public IAnalogInput A2 {
-                get { return _a2 ?? (_a2 = new SpotAnalogInput(Cpu.AnalogChannel.ANALOG_2)); }
+                get { return GetOrCreate(2); }
             }
 
             public IAnalogInput A3 {
-                get { return _a3 ?? (_a3 = new SpotAnalogInput(Cpu.AnalogChannel.ANALOG_3)); }
+                get { return GetOrCreate(3); }
             }
 
             public IAnalogInput A4 {
-                get { return _a4 ?? (_a4 = new SpotAnalogInput(Cpu.AnalogChannel.ANALOG_4)); }
+                get { return GetOrCreate(4); }
             }
 
             public IAnalogInput A5 {
-                get { return _a5 ?? (_a5 = new SpotAnalogInput(Cpu.AnalogChannel.ANALOG_5)); }
+                get { return GetOrCreate(5); }
+            }
+
+            public DigitalInputs DigitalInput {
+                get { return new DigitalInputs(); }
             }
 
             public IAnalogInput this[int index] {
                 get {
-                    switch (index) {
-                        case 0:
-                            return A0;
-                        case 1:
-                            return A1;
-                        case 2:
-                            return A2;
-                        case 3:
-                            return A3;
-                        case 4:
-                            return A4;
-                        case 5:
-                            return A5;
-                        default:
-                            throw new IndexOutOfRangeException();
+                    if (index < 0 || index > 5) {
+                        throw new IndexOutOfRangeException();
                     }
+                    return GetOrCreate(index);
                 }
+            }
+
+            private IAnalogInput GetOrCreate(int index) {
+                return _store[index] ?? (_store[index] = new SpotAnalogInput((Cpu.AnalogChannel)index));
+            }
+        }
+
+        public sealed class DigitalInputs
+        {
+            //todo: should we really be tracking these?
+            private readonly Hashtable _store = new Hashtable();
+
+            public IDigitalInput Bind(Cpu.Pin pin, string id, ResistorMode internalResistorMode, InterruptMode interruptMode, int debounceMilliseconds = 0) {
+                var input = new SpotDigitalInput(pin, id, internalResistorMode, interruptMode, debounceMilliseconds);
+                _store.Add(pin, input);
+                return input;
+            }
+
+            public IDigitalInput Get(Cpu.Pin pin) {
+                return (IDigitalInput)_store[pin];
+            }
+        }
+
+        public sealed class DigitalOutputs
+        {
+            //todo: should we really be tracking these?
+            private readonly Hashtable _store = new Hashtable();
+
+            public IDigitalOutput Bind(Cpu.Pin pin, bool initialState = false, string id = null) {
+                var output = new SpotDigitalOutput(pin, initialState, id);
+                _store.Add(pin, output);
+                return output;
+            }
+
+            public IDigitalOutput Get(Cpu.Pin pin) {
+                return (IDigitalOutput)_store[pin];
             }
         }
 
         public sealed class PwmOutputs
         {
-            private IPulseWidthModulatedOutput _d10;
-            private IPulseWidthModulatedOutput _d11;
-            private IPulseWidthModulatedOutput _d3;
-            private IPulseWidthModulatedOutput _d5;
-            private IPulseWidthModulatedOutput _d6;
-            private IPulseWidthModulatedOutput _d9;
+            private readonly IPulseWidthModulatedOutput[] _store = new IPulseWidthModulatedOutput[6];
 
             public IPulseWidthModulatedOutput D10 {
-                get { return _d10 ?? (_d10 = new SpotPwmOutput(SL.PWMChannels.PWM_PIN_D10)); }
+                get { return _store[4] ?? (_store[4] = new SpotPwmOutput(SL.PWMChannels.PWM_PIN_D10)); }
             }
 
             public IPulseWidthModulatedOutput D11 {
-                get { return _d11 ?? (_d11 = new SpotPwmOutput(SL.PWMChannels.PWM_PIN_D11)); }
+                get { return _store[5] ?? (_store[5] = new SpotPwmOutput(SL.PWMChannels.PWM_PIN_D11)); }
             }
 
             public IPulseWidthModulatedOutput D3 {
-                get { return _d3 ?? (_d3 = new SpotPwmOutput(SL.PWMChannels.PWM_PIN_D3)); }
+                get { return _store[0] ?? (_store[0] = new SpotPwmOutput(SL.PWMChannels.PWM_PIN_D3)); }
             }
 
             public IPulseWidthModulatedOutput D5 {
-                get { return _d5 ?? (_d5 = new SpotPwmOutput(SL.PWMChannels.PWM_PIN_D5)); }
+                get { return _store[1] ?? (_store[1] = new SpotPwmOutput(SL.PWMChannels.PWM_PIN_D5)); }
             }
 
             public IPulseWidthModulatedOutput D6 {
-                get { return _d6 ?? (_d6 = new SpotPwmOutput(SL.PWMChannels.PWM_PIN_D6)); }
+                get { return _store[2] ?? (_store[2] = new SpotPwmOutput(SL.PWMChannels.PWM_PIN_D6)); }
             }
 
             public IPulseWidthModulatedOutput D9 {
-                get { return _d9 ?? (_d9 = new SpotPwmOutput(SL.PWMChannels.PWM_PIN_D9)); }
-            }
-
-            public IPulseWidthModulatedOutput this[int index] {
-                get {
-                    switch (index) {
-                        case 3:
-                            return D3;
-                        case 5:
-                            return D5;
-                        case 6:
-                            return D6;
-                        case 9:
-                            return D9;
-                        case 10:
-                            return D10;
-                        case 11:
-                            return D11;
-                        default:
-                            throw new IndexOutOfRangeException();
-                    }
-                }
+                get { return _store[3] ?? (_store[3] = new SpotPwmOutput(SL.PWMChannels.PWM_PIN_D9)); }
             }
         }
     }
